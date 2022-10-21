656d3f96f25dc2bfddc0f3a73415148fece59ad6
Alexandro <alexlmangabeira@gmail.com>
2022-10-20 22:38:59 -0300

    desafio três finalizado

--- challengesThree/challengesThree.js
+++ challengesThree/challengesThree.js
@@ -1,11 +1,31 @@
-const numAleatorio = parseInt(Math.random() * (10 - 1) + 1);
+const readline = require('readline-sync');
+const numAleatorio = parseInt(Math.random() * (20 - 1) + 1);
 
 const numSorte = (palpite) => {
     if (palpite > numAleatorio) {
-        return 'Tente um número menor!';
+        return '\nTente um número MENOR!';
     } else if (palpite < numAleatorio) {
-        return 'Tente um número maior!';
+        return '\nTente um número MAIOR!';
     } else {
-        return 'Parabéns você acertou!!!!';
+        return '\nParabéns você acertou!!!!';
     }
 }
+
+const jogo = () => {
+    let i = 0;
+
+    while (i <= 5) {
+        console.log(`\nResta ${6 - i} tentativas`);
+        const palpite = parseInt(readline.question(`Chute seu palpite ${i + 1}: `));
+        i++;
+        const sugestao = numSorte(palpite);
+        console.log(sugestao);
+
+        if (palpite === numAleatorio) {
+            console.log('Fim de JOGO...');
+            break;
+        }
+    }
+}
+
+jogo();
