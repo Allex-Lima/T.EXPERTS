d9ef58be826a1c5e5f50512bebd1fe9b0bf57d49
Alexandro <alexlmangabeira@gmail.com>
2022-10-23 17:12:31 -0300

    acrescentando mais uma codição

--- challengesThree/challengesThree.js
+++ challengesThree/challengesThree.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
-const numAleatorio = parseInt(Math.random() * (20 - 1) + 1);
+const numAleatorio = parseInt(Math.random() * (50 - 1) + 1);
+//console.log(numAleatorio);
 
 const numSorte = (palpite) => {
     if (palpite > numAleatorio) {
@@ -18,11 +19,17 @@ const jogo = () => {
         console.log(`\nResta ${6 - i} tentativas`);
         const palpite = parseInt(readline.question(`Chute seu palpite ${i + 1}: `));
         i++;
+
+        if (i > 5 && palpite !== numAleatorio) {
+            console.log('\n\nNão foi dessa vez :)');
+            console.log(`Número sorteado: ${numAleatorio}`);
+            break;
+        }
+
         const sugestao = numSorte(palpite);
         console.log(sugestao);
 
         if (palpite === numAleatorio) {
-            console.log('Fim de JOGO...');
             break;
         }
     }
