1caca5878d5163850cf9a9468eaad6fb037bdff4
Alexandro <alexlmangabeira@gmail.com>
2022-11-09 23:07:29 -0300

    Desafio concluido

--- desafio/index.js
+++ desafio/index.js
@@ -1,7 +1,8 @@
-
 function sorteioNumero() {
-    let x = 0, ultimosSorteios = [], numeroSorteado, msn
-    
+    let x = 0, acumulador = 0, numeroSorteado, msn, msnDois
+    let ultimosSorteios = new Array(3).fill(0)
+    let faseDoisPA = 0, faseTresPA = 0
+
     while (x < 3) {
         let arrayNumero = new Array(3).fill(0)
         let i = 0
@@ -13,20 +14,24 @@ function sorteioNumero() {
             }
             i++
         }
-        ultimosSorteios.push(arrayNumero)
-        console.log(ultimosSorteios);
-        for (let index = 0; index < arrayNumero.length - 1; index++) {
-            if (arrayNumero[index] === arrayNumero[index + 1]) {
-                msn = `${arrayNumero} - Porta Aberta`
-            } else {
-                msn = `${arrayNumero} - Tente Novamente`
-                break
-            }
-        }
+
+        const elementAarrayIguais = arrayNumero.every(item => item === arrayNumero[arrayNumero.length - 1])
+
+        msn = elementAarrayIguais ? `Fase 2: ${++faseDoisPA} Portas Aberta ` : `Fase 2: Tente de novo`
+
+        //Fase 3
+        
+        const arrayIguais = ((arrayNumero.length === ultimosSorteios.length) && (arrayNumero.every((item, index) => item === ultimosSorteios[index])))
+        
+        ultimosSorteios = [...arrayNumero]
+        
+        msnDois = arrayIguais ? `Fase 3: Porta ${++faseTresPA} Aberta` : `Fase 3 - Tente novamente`
         x++
     }
-    return msn
+    acumulador = (faseDoisPA + faseTresPA)
+
+    return `${msn} \n${msnDois} \nAcumulador: ${acumulador}`
 }
 
-const x = sorteioNumero()
-console.log(x);
+const obterSorteio = sorteioNumero()
+console.log(obterSorteio);
